767ae1623d243caaf3fcabb923364e5dbacf8717
Samuel Luciano <samuel.adm0017@gmail.com>
2025-08-25 16:05:34 -0100

    Create Variavel.js

---
+++ Fundamentos-do-JavaScript/Variavel.js
@@ -0,0 +1,14 @@
+let nome = "Samuel";
+//Aqui foi declarado uma variável let que terá o nome de "nome".
+//nome recebe "Samuel" que é uma string de 6 caracteres.
+// O sinal de = é para atribuição do valor.
+
+const idade = 25;
+//const é um tipo de variável fixo que terá um valor único.
+//idade é nome a qual será chamado, recebendo o valor 25 do tipo number.
+
+console.log(nome);
+console.log(idade);
+//Aqui Nós temos um console log que tem o seu objetivo de imprimir
+//O resultado na tela, chamando as variáveis.
+nome = "Luciano";
