d564fa224bcdc79a7f0585ce32bae2f2cc67c5a8
Samuel Luciano <samuel.adm0017@gmail.com>
2025-08-25 16:16:07 -0100

    Create Tipo-de-Dados.js

---
+++ Fundamentos-do-JavaScript/Tipo-de-Dados.js
@@ -0,0 +1,10 @@
+
+let texto = "Olá";
+let numero = 22;
+let booleano = true;
+let array = [1, 2, 3];
+let objeto = { nome: "João", idade: 22 };
+let nulo = null;
+let indefinido;
+
+console.log(texto, numero, booleano, array, objeto, nulo, indefinido);
