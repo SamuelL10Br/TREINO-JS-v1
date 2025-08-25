87912833e9d3720ca8109ac528234d16d01618e4
Samuel Luciano <samuel.adm0017@gmail.com>
2025-08-25 16:47:00 -0100

    Create AtividadeV1.js

---
+++ Fundamentos-do-JavaScript/AtividadeV1.js
@@ -0,0 +1,26 @@
+//Console cidade
+let cidade = "Braga";
+console.log(cidade);
+
+//Console variáveis e operações
+let x = 10;
+let y = 5;
+console.log(x + y);
+console.log(x - y);         
+console.log(x * y);
+console.log(x / y);
+
+//Console condição
+let idade = 18;
+if (idade >= 18) {
+    console.log("Pode dirigir");
+} else {
+    console.log("Não pode dirigir");
+}
+
+//Console função
+function dobro(numero) {
+    return numero * 2;
+}
+dobro(5);
+console.log(dobro(5));
