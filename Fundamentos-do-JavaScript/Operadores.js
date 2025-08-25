d4f84274145c4d12a5f9ad03d3ad55ccf475deae
Samuel Luciano <samuel.adm0017@gmail.com>
2025-08-25 16:25:31 -0100

    Create Operadores.js

---
+++ Fundamentos-do-JavaScript/Operadores.js
@@ -0,0 +1,10 @@
+console.log(5 + 3);
+console.log(10 > 5);
+console.log(10 === "10");
+console.log(10 == "10");
+console.log(true && false);
+
+//Operadores
+//Aritméticos: +, -, *, /, %
+//Comparação: ==, ===, !=, !==, >, <, >=, <=
+//Lógicos: && (E), || (OU), ! (NÃO)
