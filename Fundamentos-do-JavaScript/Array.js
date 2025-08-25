aef40d22378155cdd06ecb53c71b85a600d3324a
Samuel Luciano <samuel.adm0017@gmail.com>
2025-08-25 21:50:37 -0100

    Create Array.js

---
+++ Fundamentos-do-JavaScript/Array.js
@@ -0,0 +1,15 @@
+// Array
+let frutas = ["maça", "banana", "uva", "maracujá"];
+console.log(frutas[1]); //Mostrar uma fruta na primeira posição
+console.log(frutas.length); //Mostrar o comprimento do array ou seja quantas
+//posições tem.
+
+// Objeto
+let pessoa = {
+    nome: "Samuel",
+    idade: 22,
+    cidade: "Braga"
+};
+console.log(pessoa.nome);
+console.log(pessoa["idade"]);
+console.log(pessoa.cidade);
